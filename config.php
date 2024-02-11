16ec2f7c9754ab74c2898e54a39ae6fc43074322
Ephraim <ebhlert@gmail.com>
2024-02-11 11:53:34 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// Basic connection settings
+$databaseHost = '127.0.0.1';
+$databaseUsername = 'root';
+$databasePassword = '30515123';
+$databaseName = 'rolodex';
+
+// Connect to the database
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
